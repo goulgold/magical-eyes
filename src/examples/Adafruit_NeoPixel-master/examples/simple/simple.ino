3394e91b38df65c6287cb19bc7d89904bf730213
Goulgold <goulgold@gmail.com>
2014-11-25 22:59:44 -0500

    led example

--- src/examples/Adafruit_NeoPixel-master/examples/simple/simple.ino
+++ src/examples/Adafruit_NeoPixel-master/examples/simple/simple.ino
@@ -13,19 +13,32 @@
 // example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int delayval = 500; // delay for half a second
+int delayval = 30; // delay for half a second
+
+// For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+int x=15;
 
 void setup() {
   pixels.begin(); // This initializes the NeoPixel library.
 }
 
 void loop() {
-  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
-  int x=15;
-  for(int i=0;i<NUMPIXELS&&(x<255);i++){
+
+  for(int i=0;i<NUMPIXELS;i++){
+    x=(x+1)%120; //The amptitude of LED    
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(x,0,0)); // Moderately bright green color.
-    x=x+10;
+    if ( x < 40 )
+    {
+        pixels.setPixelColor(i, pixels.Color(40-x,x,0)); // Moderately bright green color.
+    }
+    else if ( x < 80 )
+   {
+        pixels.setPixelColor(i, pixels.Color(0,80-x,x-40)); // Moderately bright green color.
+   } 
+   else
+   {
+        pixels.setPixelColor(i, pixels.Color(x-80,0,120-x)); // Moderately bright green color.
+   }
     pixels.show(); // This sends the updated pixel color to the hardware.
     delay(delayval); // Delay for a period of time (in milliseconds).
   }
